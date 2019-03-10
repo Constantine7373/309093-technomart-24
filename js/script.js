af6bc4c4c84e07793994c7d7c703a7fa91955ad5
Alexandr Lukichyov <constantine73@yandex.ru>
2019-03-10 14:24:58 +0400

    Secon attempt
    base and advanced criteries are fixed

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ var writeUsClose = document.querySelector(".popup-close-btn-write-us");
 var itemBuy = document.querySelectorAll(".catalog-item-popup-buy");
 var itemBuyPopup = document.querySelector(".popup-cart")
 var itemBuyClose = document.querySelector(".popup-cart-continue");
+var radToggle = document.getElementsByName("categories-slider-toggle");
+var radButtonNext = document.querySelector(".categories-slider-btn-next");
+var radButtonPrev = document.querySelector(".categories-slider-btn-prev");
 
 if (mapLink !== null) {
   mapLink.addEventListener("click", function(evt) {
@@ -51,3 +54,67 @@ if (itemBuyPopup !== null) {
     itemBuyPopup.style.display = "none";
   })
 };
+
+// if (radToggle !== null) {
+//   for (var i = 0; i < radToggle.length; i++) {
+//     if (radToggle[i].checked) {
+//       var currentToggle = radToggle[i];
+//       var nextToggle = radToggle[i + 1];
+//       radToggle.length = i;
+//     };
+//   };
+// };
+
+radButtonNext.addEventListener("click", function() {
+  if (radToggle !== null) {
+    for (var i = 0; i < radToggle.length; i++) {
+      if (radToggle[i].checked) {
+        var currentToggle = radToggle[i];
+        var nextToggle = radToggle[i + 1];
+        radToggle.length = i;
+      };
+    };
+  };
+  if (currentToggle === radToggle[radToggle.length - 1]) {
+    console.log(radToggle.length);
+    i = 0;
+    radToggle[i].checked = true;
+    currentToggle = radToggle[i];
+    nextToggle = radToggle[i + 1];
+    i++;
+  } else {
+    console.log("нажал кнопку");
+    if (i = radToggle.length) {
+      i = (radToggle.length - 2);
+    }
+    nextToggle.checked = true;
+    currentToggle = radToggle[i + 1];
+  };
+});
+
+radButtonPrev.addEventListener("click", function() {
+  if (radToggle !== null) {
+    for (var i = 0; i < radToggle.length; i++) {
+      if (radToggle[i].checked) {
+        var currentToggle = radToggle[i];
+        var prevToggle = radToggle[i - 1];
+        radToggle.length = i;
+      };
+    };
+  };
+  if (currentToggle === radToggle[0]) {
+    console.log(radToggle.length);
+    i = (radToggle.length - 1);
+    radToggle[i].checked = true;
+    currentToggle = radToggle[i];
+    prevToggle = radToggle[i - 1];
+    i--;
+  } else {
+    console.log("нажал кнопку");
+    if (i = radToggle.length) {
+      i = (radToggle.length - 2);
+    }
+    prevToggle.checked = true;
+    currentToggle = radToggle[i + 1];
+  };
+});
